42a7dc68d61b42513dfce740cea6a08b6f98b5d0
tmitev <todor.mitev@sofairo.com>
2025-10-02 17:22:58 +0300

    corrections_3_0

--- mailing_service/views.py
+++ mailing_service/views.py
@@ -101,6 +101,16 @@ class MessageListView(LoginRequiredMixin, ListView):
     template_name = 'mailing_service/messages_list.html'
     context_object_name = 'messages'
 
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_manager():
+            queryset = Message.objects.all()
+        else:
+            queryset = Message.objects.filter(owner=user)
+
+        for message in queryset:
+            message.update_status()
+        return queryset
 
 @method_decorator(cache_page(60 * 15), name='dispatch')
 class MailingListView(LoginRequiredMixin, ListView):
